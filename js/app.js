22e4321ce7fd7afceaffe2dc8122220e332115ad
Jaime Pillora <jpillora@gmail.com>
2013-09-18 22:03:00 +1000

    updated

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 (function() {
-  console.log('run');
+  var $, link, linkId, load, _i, _len;
+
+  linkId = window.location.hash.substr(1);
+
+  $ = function(id) {
+    return document.getElementById(id);
+  };
+
+  load = function(link) {
+    $('link-title').innerHTML = link.title;
+    $('link-url').innerHTML = link.url;
+    $('popup').className = 'active';
+    return setTimeout(function() {
+      return window.location.href = link.url;
+    }, 2000);
+  };
+
+  if (linkId) {
+    for (_i = 0, _len = LINKS.length; _i < _len; _i++) {
+      link = LINKS[_i];
+      if (link.id === linkId) {
+        load(link);
+        break;
+      }
+    }
+  }
 
 }).call(this);
--- src/scripts/run.coffee
+++ src/scripts/run.coffee
@@ -1,2 +1,20 @@
 
-console.log 'run'
+
+linkId = window.location.hash.substr(1)
+
+$ = (id) -> document.getElementById id
+
+load = (link) ->
+
+  $('link-title').innerHTML = link.title
+  $('link-url').innerHTML = link.url
+  $('popup').className = 'active'
+  setTimeout ->
+    window.location.href = link.url
+  , 2000
+
+if linkId
+  for link in LINKS
+    if link.id is linkId
+      load link
+      break
